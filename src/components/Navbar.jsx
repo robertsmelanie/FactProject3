91da0623601663acf2e09072a37fdf4a949f769d
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-03-10 14:36:30 -0400

    pushing basename

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function Navbar () {
   return (
     <nav>
     <div>
     <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/Fact1">Fact 1</a></li>
-        <li><a href="/Fact2">Fact 2</a></li>
-        <li><a href="/Fact3">Fact 3</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Fact1">Fact 1</Link></li>
+        <li><Link to="/Fact2">Fact 2</Link></li>
+        <li><Link to="/Fact3">Fact 3</Link></li>
     </ul>
 
 
