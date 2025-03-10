f01e25348b37df39c93c8287085f66f78ca9c89f
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-03-10 08:09:34 -0400

    update

--- src/components/BgImage.jsx
+++ src/components/BgImage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function BgImageComponent() {
-  return <img src="/bg-image.png" alt="Background" />;
+  // return <img src="1Artblue37.png" alt="Background" />;
 }
 
 export default BgImageComponent;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Home () {
   return (
     <div>
-    <h4>Home</h4>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dignissimos hic facilis eius, quas impedit perferendis rerum ex soluta, voluptate ratione distinctio quaerat? Officia eaque consequatur ratione eum qui est!</p>
+    <h4>Welcome to Fun Facts!</h4>
+    <p>Click on a fact in the navbar to learn more!</p>
 
 
     </div>
